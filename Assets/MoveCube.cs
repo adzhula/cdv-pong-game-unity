701a7afce83a40d5b90b7baca558b17f063e4a72
adzhula <adzhula@edu.cdv.pl>
2023-03-29 16:10:29 +0200

    Created 2 players, added movement scripts, key and position validation

--- Assets/MoveCube.cs
+++ Assets/MoveCube.cs
@@ -5,24 +5,38 @@ using UnityEngine;
 public class MoveCube : MonoBehaviour
 {
 
-    public Rigidbody rigidBody;
-    
+    public Rigidbody2D rigidBody2D;
+    public KeyCode upKey;
+    public KeyCode downKey;
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        rigidBody = GetComponent<Rigidbody>();
-    
+        rigidBody2D = GetComponent<Rigidbody2D>();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        if (Input.GetKey(upKey) && transform.position.y < 5)
         {
-            rigidBody.AddForce(Vector3.up * 20, ForceMode.Impulse);
-            
+            rigidBody2D.velocity = Vector2.up * speed;
         }
- 
+        else if (Input.GetKey(downKey) && transform.position.y > -5)
+        {
+            rigidBody2D.velocity = Vector2.down * speed;
+        }
+        else 
+        {
+            rigidBody2D.velocity = Vector2.zero;
+        }
+
+
     }
+
 
 }
